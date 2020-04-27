4f9a388ca6a248bc6a61dc12ea9668270da207fa
Catriona <catrionamairi@blueyonder.co.uk>
2020-04-27 17:04:12 +0100

    added cat image in app file

---
+++ app.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+require 'shotgun'
+
+set :session_secret, 'super secret'
+
+get '/' do
+  "Hello World!!"
+end
+
+get '/secret' do
+  "Secret hello!!"
+end
+
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+  <img src='http://bit.ly/1eze8aE'>
+  </div>"
+end
